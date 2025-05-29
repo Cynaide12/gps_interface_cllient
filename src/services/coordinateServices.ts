aec0d477a0a58ecb1e9fee07a92647ce255257d4
Cynaide12 <114859811+Cynaide12@users.noreply.github.com>
2025-05-29 22:47:15 +0500

    -

--- src/services/coordinateServices.ts
+++ src/services/coordinateServices.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import type { CoordinatesResponse } from "./types";
 
 export const getLastCoordinate = async (): Promise<CoordinatesResponse> => {
-  const response = await axios.get("http://localhost:8080/get_last_coordinates");
+  const response = await axios.get("http://localhost:8080/api/get_last_coordinates");
   return response.data;
 };
